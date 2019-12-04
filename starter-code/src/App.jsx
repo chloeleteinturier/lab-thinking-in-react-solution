ff43d9f82405bb719bd4391b987bd9f22ddb460c
Chloe <chloe@Chloes-MacBook-Air.local>
2019-12-04 09:48:53 +0100

    done

--- starter-code/src/App.jsx
+++ starter-code/src/App.jsx
@@ -1,18 +1,45 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+
 import './App.css';
 
+import productsList from './data.json';
+import Table from './components/Table';
+import SearchBar from './components/Search'
+
 export default class App extends Component {
+  constructor(props){
+    super(props);
+
+    this.state={
+      items : productsList.data,
+      visibleItems: productsList.data,
+    }
+  }
+
+  refineItemList = (query, showUnStocked) => {
+
+    let clone = [...this.state.items];
+
+    let newList = clone.filter(eachThing => {
+      if(showUnStocked) return eachThing.name.toUpperCase().includes(query.toUpperCase());
+    else return eachThing.stocked && eachThing.name.toUpperCase().includes(query.toUpperCase()); 
+    })
+    // console.log(newList);
+    this.setState({
+      visibleItems: newList
+    })
+  }
+
+
   render() {
+    // console.log(this.state);
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+      <div >
+        <h1>Welcome to IronStore</h1>
+        <SearchBar 
+          doTheSearch = {this.refineItemList} 
+        />
+        <Table allItems = {this.state.visibleItems} />
       </div>
     );
   }
